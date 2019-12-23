f98a5852b6d58f2ecd939573e57475274a1c3cb5
biotein1 <50307797+biotein1@users.noreply.github.com>
2019-12-23 07:36:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ NOTE: This is the custom jQuery file for the template
         var element = $(".element");
         $(function() {
             element.typed({
-                strings: ["Senescent cell removal.", "Cure for biological aging.", "Dedicated team."],
+                strings: ["Cure for EBS-DM.", "Cure for aging-related diseases.", "Passionate team."],
                 typeSpeed: 100,
                 loop: true,
             });
